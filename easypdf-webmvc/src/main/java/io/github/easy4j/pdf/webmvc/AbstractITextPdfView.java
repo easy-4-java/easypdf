a95d4abfea0bd73fd08b281817e48edfd78ef4f3
wandl-6A72h <hiwepy@gmail.com>
2026-08-07 22:51:02 +0800

    build: align 3.0.x dependencies with ddd4j

--- easypdf-webmvc/src/main/java/io/github/easy4j/pdf/webmvc/AbstractITextPdfView.java
+++ easypdf-webmvc/src/main/java/io/github/easy4j/pdf/webmvc/AbstractITextPdfView.java
@@ -4,8 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.view.AbstractView;
 
